283a50ead69bafaac71a43366d548de614d7c58d
drwwtck <tuck757@wearescpps.org>
2026-09-23 14:36:52 +0000

    second platformer commit

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -18,7 +18,13 @@ $(function () {
 
     // Create walls - do not delete or modify this code
     createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+    createPlatform(
+      -50,
+      canvas.height - 10,
+      canvas.width + 100,
+      200,
+      "rgb(118, 0, 233)",
+    ); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
 
@@ -29,31 +35,29 @@ $(function () {
     // TODO 1 - Enable the Grid
     // toggleGrid();
 
-
     // TODO 2 - Create Platforms
 
-createPlatform(600, 650, 200, 10, "#9effff");
-createPlatform(900, 550, 100, 10, "#9effff");
-createPlatform(410, 410, 300, 10, "#9effff");
-createPlatform(200, 300, 100, 10, "#9effff");
-createPlatform(310, 175, 600, 10, "#9effff");
-createPlatform(1000, 200, 100, 10, "#9effff");
-createPlatform(1200, 150, 200, 10, "#9effff");
-createFakePlatform(800, 450, 30, 10, "#9effff" );
-createBadPlatform(460, 700, 10, 10,"lightblue");
+    createPlatform(600, 650, 200, 10, "#9effff");
+    createPlatform(900, 550, 100, 10, "#9effff");
+    createPlatform(410, 410, 300, 10, "#9effff");
+    createPlatform(200, 300, 100, 10, "#9effff");
+    createPlatform(310, 175, 600, 10, "#9effff");
+    createPlatform(1000, 200, 100, 10, "#9effff");
+    createPlatform(1200, 150, 200, 10, "#9effff");
+    createFakePlatform(800, 450, 30, 10, "#9effff");
+    createBadPlatform(460, 700, 10, 10, "lightblue");
 
-  // TODO 3 - Create Collectables
+    // TODO 3 - Create Collectables
 
-createCollectable("max", 200, 550, 0, 0);
-createCollectable("steve", 600, 200, 0, 0);
-createCollectable("diamond", 1350, 100, 0, 0);
+    createCollectable("max", 200, 550, 0, 0);
+    createCollectable("steve", 600, 200, 0, 0);
+    createCollectable("diamond", 1350, 100, 0, 0);
 
-  // TODO 4 - Create Cannons
+    // TODO 4 - Create Cannons
 
-createCannon("right", 650, 2000);
-createCannon("left", 50, 2000);
-createCannon("bottom", 400, 3000);
-
+    createCannon("right", 650, 2000);
+    createCannon("left", 50, 2000);
+    createCannon("bottom", 400, 3000);
   }
 
   registerSetup(setup);
